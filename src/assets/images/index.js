2d7895d086a9accea3cbe0a7729049d63da2f73c
faizanthisworld <149044542+faizanthisworld@users.noreply.github.com>
2023-10-26 12:21:24 +0530

    Add files via upload

---
+++ src/assets/images/index.js
@@ -0,0 +1,15 @@
+export const House = require("./house.jpg");
+export const Sunflower = require("./sunflower.jpg");
+export const Tiger = require("./tiger.jpg");
+export const Amazon = require("./amazon.png")
+export const Facbook = require("./facebook.png")
+export const Whatsapp = require("./whatsapp.png")
+export const Linkedin = require("./linkedin.png")
+export const Twitter = require("./twitter.jpg")
+export const Google = require("./Google.png")
+export const Apple = require("./apple.png")
+ 
+
+
+
+
